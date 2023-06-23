d628570816a3af16a0c2940dc4495afd293c56c9
Toru Fujita <tr.fujita@outlook.com>
2023-06-23 17:39:14 +0900

    format files, but no changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
@@ -14,7 +14,7 @@
 		xsmall: '(max-width: 480px)'
 	});
 
-	$(function() {
+	$(function () {
 
 		var $body = $('body'),
 			$header = $('#header'),
@@ -22,91 +22,91 @@
 			$wrapper = $('#wrapper');
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+		skel.on('+medium -medium', function () {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
 		// Header.
-			var ids = [];
+		var ids = [];
 
-			// Set up nav items.
+		// Set up nav items.
+		$nav_a
+			.scrolly({ offset: 44 })
+			.on('click', function (event) {
+
+				var $this = $(this),
+					href = $this.attr('href');
+
+				// Not an internal link? Bail.
+				if (href.charAt(0) != '#')
+					return;
+
+				// Prevent default behavior.
+				event.preventDefault();
+
+				// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
 				$nav_a
-					.scrolly({ offset: 44 })
-					.on('click', function(event) {
+					.removeClass('active')
+					.addClass('scrollzer-locked');
 
-						var $this = $(this),
-							href = $this.attr('href');
+				// Set active class on this link.
+				$this.addClass('active');
 
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
+			})
+			.each(function () {
 
-						// Prevent default behavior.
-							event.preventDefault();
+				var $this = $(this),
+					href = $this.attr('href'),
+					id;
 
-						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
+				// Not an internal link? Bail.
+				if (href.charAt(0) != '#')
+					return;
 
-						// Set active class on this link.
-							$this.addClass('active');
+				// Add to scrollzer ID list.
+				id = href.substring(1);
+				$this.attr('id', id + '-link');
+				ids.push(id);
 
-					})
-					.each(function() {
+			});
 
-						var $this = $(this),
-							href = $this.attr('href'),
-							id;
-
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
-
-						// Add to scrollzer ID list.
-							id = href.substring(1);
-							$this.attr('id', id + '-link');
-							ids.push(id);
-
-					});
-
-			// Initialize scrollzer.
-				$.scrollzer(ids, { pad: 300, lastHack: true });
+		// Initialize scrollzer.
+		$.scrollzer(ids, { pad: 300, lastHack: true });
 
 		// Off-Canvas Navigation.
 
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#header" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
-					'</div>'
-				)
-					.appendTo($body);
+		// Title Bar.
+		$(
+			'<div id="titleBar">' +
+			'<a href="#header" class="toggle"></a>' +
+			'<span class="title">' + $('#logo').html() + '</span>' +
+			'</div>'
+		)
+			.appendTo($body);
 
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'right',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
+		// Header.
+		$('#header')
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'header-visible'
+			});
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #header, #wrapper')
-						.css('transition', 'none');
+		// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+		if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+			$('#titleBar, #header, #wrapper')
+				.css('transition', 'none');
 
 	});
 
